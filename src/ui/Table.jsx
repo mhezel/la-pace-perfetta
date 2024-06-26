4c8ca66754d712b198992ddf583c27b8d9aa62a0
Mhezel Mohammad <mhezelkhan@gmail.com>
2024-06-26 15:27:04 +0800

    updates

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -33,7 +33,7 @@ const StyledRow = styled(CommonRow)`
   padding: 1.2rem 2.4rem;
 
   &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
+    border-bottom: 1px solid var(--color-grey-200);
   }
 `;
 
