82f9047cde824630637bfeb97f57d301ac0f6236
Mhezel Mohammad <mhezelkhan@gmail.com>
2024-07-09 11:32:07 +0800

    Deleting a Reservation, another use-case of useTransition hook for loaders
    Deleting a Reservation, another use-case of useTransition hook for loaders

--- la-pace-perfetta-client/app/_lib/actions.js
+++ la-pace-perfetta-client/app/_lib/actions.js
@@ -3,10 +3,10 @@
 import { revalidatePath } from "next/cache"; // manual cache revalidation
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuestProfile(formData){
     // console.log(formData);
-
     const session = await auth(); //AUTHENTICATION & AUTHORIZATION
     if(!session) throw new Error ("User must be logged-in");
 
@@ -27,7 +27,28 @@ export async function updateGuestProfile(formData){
     throw new Error('Guest could not be updated');
 
     revalidatePath('/account/profile'); // manual cache revalidation || re-fetching data
-    
+}
+
+export async function deleteGuestBooking(booking_id){
+    const session = await auth(); //AUTHENTICATION & AUTHORIZATION
+    if(!session) throw new Error ("User must be logged-in");
+
+    //additional layer for authorization
+    const guestBookings = await getBookings(session.user.guest_id);
+    const guestBookingIds = guestBookings.map((booking) => booking.booking_id);
+
+    if(!guestBookingIds.includes(booking_id)) 
+        throw new Error("Action deleting booking is not allowed");
+
+    const { error } = await supabase
+    .from('tbl_bookings')
+    .delete()
+    .eq('booking_id', booking_id);
+
+    if (error) 
+        throw new Error('Booking could not be deleted');
+
+    revalidatePath('/account/reservations'); // manual cache revalidation || re-fetching data
 }
 
 export async function signInAction(){
